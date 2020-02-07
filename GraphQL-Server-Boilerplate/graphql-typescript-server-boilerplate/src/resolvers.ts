a9cab2f5191e2c1e4ed4fcb5d7312e2b98bb7a25
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-07 13:57:51 -0800

    Started implementing the TypeORM

--- GraphQL-Server-Boilerplate/graphql-typescript-server-boilerplate/src/resolvers.ts
+++ GraphQL-Server-Boilerplate/graphql-typescript-server-boilerplate/src/resolvers.ts
@@ -1,7 +1,14 @@
-export const resolvers = {
+import { ResolverMap } from "./types/graphql-utils"
+
+export const resolvers: ResolverMap = {
   Query: {
-    hello: (_: any, { name }: any) => `Bye ${name || 'World'}`,
+    hello: (_: any, { name }: GQL.IHelloOnQueryArguments) => `Bye ${name || 'World'}`,
   },
+
+  Mutation: {
+    register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => {}
+  }
+
 }
 
 // we use a library called GQL2TS to get the type of the arguments
